978ac9c3004602b26607cbf057bfa791702bb510
x <x@xxxxxxx.dev>
2023-04-12 17:33:55 +0200

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alefunion/reverseproxy
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"strings"
+	"time"
+)
+
+func spaceOrTab(r rune) bool {
+	return r == ' ' || r == '\t'
+}
+
+// Origin hosts mapped to target hosts
+var hostmap = make(map[string]string)
+
+func init() {
+	// Help
+	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "-help" || os.Args[1] == "help") {
+		print(`Alef Union Reverse Proxy
+
+Usage:
+
+	Create a "hostmap" file containing an origin host to target host mapping (1 per line).
+	Example:
+		alefunion.com     localhost:8000
+		api.alefunion.com localhost:8001
+
+`)
+		os.Exit(0)
+	}
+
+	// Make hostmap
+	f, err := os.Open("hostmap")
+	if err != nil {
+		fmt.Println("hostmap file not found")
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		l := s.Text()
+		fields := strings.FieldsFunc(l, spaceOrTab)
+		if len(fields) != 2 {
+			fmt.Printf("hostmap must have 2 fields per line (origin and target), not %d: %q\n", len(fields), l)
+			os.Exit(1)
+		}
+		hostmap[fields[0]] = fields[1]
+	}
+}
+
+func main() {
+	proxy := &httputil.ReverseProxy{Director: func(r *http.Request) {
+		target := hostmap[r.Host]
+		r.URL.Scheme = "http"
+		r.URL.Host = target
+	}}
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, ok := hostmap[r.Host]; !ok {
+			http.Redirect(w, r, "https://www.alefunion.com", http.StatusTemporaryRedirect)
+			return
+		}
+		proxy.ServeHTTP(w, r)
+	})
+
+	srv := &http.Server{
+		Addr:         ":8888",
+		Handler:      handler,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	log.Fatalln(srv.ListenAndServe())
+}
